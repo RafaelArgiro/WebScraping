05083c492ec68755b726f6f7f4e3c2c661e85e6b
Rafael Argiro <rafael.argiro@gmail.com>
2023-09-30 17:15:56 +0200

    Add export data to csv file

--- woonstad_offers.py
+++ woonstad_offers.py
@@ -1,6 +1,8 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import datetime
 from tabulate import tabulate
 
 
@@ -68,7 +70,6 @@ if nr_of_offers != total_count:
     raise Warning("Number of offers scraped not equal to total nr of offers.")
 
 
-
 # ------------------------------------------------------------------
 # -------------------- Convert dict to panda
 # ------------------------------------------------------------------
@@ -89,5 +90,30 @@ for offer in offers_data:
     offers_pd = pd.concat([offers_pd, offer_pd], axis=1)
 
 
-print(offers_pd)
+# ------------------------------------------------------------------
+# -------------------- Save to text file
+# ------------------------------------------------------------------
+
+# Get date and time stamp
+datetime_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Format datetime string
+datetime_str = datetime_str.replace(':', '.')
+datetime_str = datetime_str.replace(' ', '_')
+datetime_str = datetime_str.replace('-', '.')
+
+# Get current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Specify directory to save the file
+directory = 'Woonstad_Offer_Data'
+
+# Construct file name
+file_name = 'Offers_' + datetime_str + '.txt'
+
+# Construct full path
+file_path = os.path.join(current_dir, directory, file_name)
+
+# Safe offer data to '.txt' file in csv format
+offers_pd.to_csv(file_path, sep=',', header=False, index=False)
 
